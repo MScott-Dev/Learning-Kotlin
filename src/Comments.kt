e35de3e84dd35c34c2f1a2d944b84c34ac4bfc42
MScott-Dev <MScott0199@gmail.com>
2024-02-22 16:04:54 -0500

    Comments

---
+++ src/Comments.kt
@@ -0,0 +1,12 @@
+fun main() {
+//    This will add a single line comment
+    println("Hello, please add a comment to me!")
+    /*
+    This
+    is
+    a
+    multi
+    line
+    comment
+     */
+}
--- src/ReservedKeywords.kt
+++ src/ReservedKeywords.kt
@@ -1,5 +1,6 @@
 fun main() {
-    var brand = "Google"
+    val brand = "Google"
 //    var is a reserved keyword, so you cannot use it as a variable name
 //    var var = "Google" is invalid
+    println(brand)
 }
