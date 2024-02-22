e289706c61af274ff7daf658680c9819ace96cc9
MScott-Dev <MScott0199@gmail.com>
2024-02-22 16:48:07 -0500

    var vs val

---
+++ src/VarVSVal.kt
@@ -0,0 +1,9 @@
+fun main() {
+//    var value can be changed
+    var brand = "Google"
+    brand = "Dodge"
+    println(brand)
+//    val cannot be changed
+    val brand2 = "Subaru"
+    println(brand2)
+}
