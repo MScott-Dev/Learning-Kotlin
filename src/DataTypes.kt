20ce72aeeee5c92c7df62279ce281e557b22cd39
MScott-Dev <MScott0199@gmail.com>
2024-02-22 16:38:31 -0500

    DataTypes

---
+++ src/DataTypes.kt
@@ -0,0 +1,9 @@
+fun main() {
+    var number: Int = 20
+    var l: Long = 100L
+    var d: Double = 3.14
+    var f: Float = 3.14F
+    var b: Boolean = true
+    var s: String = "String"
+    var c: Char = 'C'
+}
