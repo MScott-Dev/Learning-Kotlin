e34250356790ca7d05381c8b514ae8059caea346
MScott-Dev <MScott0199@gmail.com>
2024-02-22 16:08:37 -0500

    Println

---
+++ src/Println.kt
@@ -0,0 +1,9 @@
+fun main() {
+//    each of these will add a new line then continue
+    println("This will output a line of text")
+    println(27)
+    println("-------")
+//    this will not add a new line
+    print("Hello, ")
+    print("How are you?")
+}
