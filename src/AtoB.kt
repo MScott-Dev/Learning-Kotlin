e4c572098298ecc1ba1071beb62100a94352eb28
MScott-Dev <MScott0199@gmail.com>
2024-07-30 18:31:20 -0400

    AtoB

---
+++ src/AtoB.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val number = "10"
+    println(10 + number.toInt())
+    val number2 = 10
+    println(10.toString() + number)
+    val d = 3.0
+    println(d)
+    println(d.toInt())
+}
