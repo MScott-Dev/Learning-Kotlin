d5162fa6bd03127028a20c9cd95829b0f3609b92
MScott-Dev <MScott0199@gmail.com>
2024-08-07 18:32:44 -0400

    Ifstatements

---
+++ src/IfStatements.kt
@@ -0,0 +1,16 @@
+fun main() {
+ val n1 = 10
+ val n2 = 20
+
+    if (n1 > n2) {
+        println("It's true")
+    }
+
+    if (n1 >= n1 && n1 <= 30) {
+        println("This is true")
+    }
+
+    if (n1 >= n1 || n1 <= 30) {
+        println("One is true")
+    }
+}
