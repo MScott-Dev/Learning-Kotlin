d0ad703a124997eccaac80b97ec2b1e16033768e
MScott-Dev <MScott0199@gmail.com>
2024-08-07 20:48:15 -0400

    WhenExpression

---
+++ src/WhenExpressionStatement.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val input = "M"
+
+    when(input) {
+        "F" -> println("Female")
+        "M" -> println("Male")
+        else -> {
+            println("Unknown")
+        }
+    }
+}
