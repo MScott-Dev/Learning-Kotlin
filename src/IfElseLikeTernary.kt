4f735140136ba8a45ecb04e5c3390a0416ee11d6
MScott-Dev <MScott0199@gmail.com>
2024-08-07 20:43:01 -0400

    Ternary

---
+++ src/IfElseLikeTernary.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val n1 = 31
+    val n2 = 20
+    var result = if (n1 >= n2) "first Ternary" else "Second Ternary"
+    var sum = if (n1 == 30) "this is true" else "This is false"
+    println(result)
+    println(sum)
+}
