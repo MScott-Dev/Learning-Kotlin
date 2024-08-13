c8ff9db94d34192ae45d235ca1ff79e61d8a4537
MScott-Dev <MScott0199@gmail.com>
2024-08-12 20:31:42 -0400

    Lists

---
+++ src/Lists.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val names: List<String> = listOf("Calyse", "Matt", "Glenn", "Sean")
+
+    println(names)
+    println(names[0])
+    println(names.size)
+    println(names.indexOf("Matt"))
+}
+
+
