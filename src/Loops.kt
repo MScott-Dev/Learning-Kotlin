08d4c26ed1571552a78431bcdab6118eec33e0a3
MScott-Dev <MScott0199@gmail.com>
2024-08-13 20:37:28 -0400

    Loops

---
+++ src/Loops.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val names = listOf("Calyse", "Matt", "Sean")
+    val numbers = intArrayOf(1, 2, 3)
+
+    for (name in names) {
+        println(name)
+    }
+    println("----")
+    for (number in numbers) {
+        println(number)
+    }
+}
