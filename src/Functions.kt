5b60717b69e4b029e05332e22d700b83219b6ffe
MScott-Dev <MScott0199@gmail.com>
2024-08-23 19:38:39 -0400

    argument defaults

--- src/Functions.kt
+++ src/Functions.kt
@@ -3,12 +3,17 @@ fun main() {
     quote()
     welcome(age=25, name="Matthew")
     quote()
+    welcome("Glenn")
+    quote()
 }
 
-fun welcome(name: String, age: Int) {
+fun welcome(name: String, age: Int = -1) {
     println("Welcome $name!")
     if (age >= 16) {
         println("$name is an adult")
+    }
+    else if (age == -1) {
+        println("Age not provided")
     } else {
         println("$name is not an adult")
     }
