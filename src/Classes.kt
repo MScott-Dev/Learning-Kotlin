9ac92dd87d480fed17c8477db361008cb05c89e2
MScott-Dev <MScott0199@gmail.com>
2024-09-03 19:15:47 -0400

    Equals()

--- src/Classes.kt
+++ src/Classes.kt
@@ -1,5 +1,6 @@
 fun main() {
     val tv = SmartDevice("Samsung Smart TV QLED", "Black", 1000.0, false)
+    val tv2 = SmartDevice("Samsung Smart TV QLED", "Black", 1000.0, false)
     println("${tv.brand} is ${tv.price}")
     tv.turnOn()
     tv.getDeviceState()
@@ -7,6 +8,8 @@ fun main() {
     println("${phone.brand} has a ${phone.color} one for ${phone.price}!")
     phone.getDeviceState()
     println(tv.toString())
+    println(tv == tv2)
+    println(tv === tv2)
 }
 
 // Blueprint
@@ -31,6 +34,28 @@ class SmartDevice constructor(
 //    Generate toString()
     override fun toString(): String {
         return "SmartDevice(brand='$brand', color='$color', price=$price, state=$state)"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as SmartDevice
+
+        if (brand != other.brand) return false
+        if (color != other.color) return false
+        if (price != other.price) return false
+        if (state != other.state) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = brand.hashCode()
+        result = 31 * result + color.hashCode()
+        result = 31 * result + price.hashCode()
+        result = 31 * result + state.hashCode()
+        return result
     }
 
 }
